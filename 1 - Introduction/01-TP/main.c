9aa7d35c52c7179340226982d5c7db478d2a3191
Robin <robin.bechlem@gmail.com>
2024-11-02 18:19:33 +0100

    Errno message intégré

--- 1 - Introduction/01-TP/main.c
+++ 1 - Introduction/01-TP/main.c
@@ -4,7 +4,7 @@ int main(int argc, char** argv) {
 
     if (argc < 2) {
         printf("USAGE : ./TP1 [option]\n");
-        return 1;
+        printf("Erreur : %s\n", strerror(EINVAL));
     }
 
     if (strcmp(argv[1], "1") == 0) {
@@ -12,7 +12,7 @@ int main(int argc, char** argv) {
             copyFile(argv[2], argv[3]);
         } else {
             printf("USAGE : ./TP1 1 [srcFile] [destFile]\n");
-            return EINVAL;
+            printf("Erreur : %s\n", strerror(EINVAL));
         }
     }
     else if (strcmp(argv[1], "2") == 0) {
@@ -20,7 +20,7 @@ int main(int argc, char** argv) {
             reverse(argv[2]);
         } else {
             printf("USAGE : ./TP1 2 [srcFile]\n");
-            return EINVAL;
+            printf("Erreur : %s\n", strerror(EINVAL));
         }
     }
     else if (strcmp(argv[1], "3") == 0) {
@@ -28,7 +28,7 @@ int main(int argc, char** argv) {
             ls(argv[2]);
         } else {
             printf("USAGE : ./TP1 3 [srcFolder]\n");
-            return EINVAL;
+            printf("Erreur : %s\n", strerror(EINVAL));
         }
     }    
     else if (strcmp(argv[1], "4") == 0) {
@@ -49,15 +49,14 @@ int main(int argc, char** argv) {
         
         } else {
             printf("USAGE : ./TP1 4 [src] [dest]\n");
+            printf("Erreur : %s\n", strerror(EINVAL));
         }
     
     }
     else {
         printf("Option inconnue : %s\n", argv[1]);
         printf("USAGE : ./TP1 [1|2|3] [src|number] [dest]\n");
+        printf("Erreur : %s\n", strerror(EINVAL));
     }
-
-    
-
     return 0;
 }
