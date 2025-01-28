f8235c7d57048e4ff365a78922c91997331a26ef
Robin <robin.bechlem@gmail.com>
2025-01-28 21:40:38 +0100

    💪🏻 First thread, and receive the command with a max lenght of 1024.

--- Projet Shell/myShell/main.c
+++ Projet Shell/myShell/main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 1024
+
+void start_main_program() {
+    printf("Main program started\n");
+}
+
+void finish_main_program() {
+    printf("Main program finished\n");
+}
 
 int main() {
-    printf("Hello, world!\n");
+    char command[MAX_COMMAND_LENGTH];
+
+    start_main_program();
+
+    while (1) {
+        printf("my_sh> ");
+        fflush(stdout);
+
+        if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+            printf("\n");
+            break;
+        }
+
+        command[strcspn(command, "\n")] = '\0';
+
+        if (strlen(command) == 0) {
+            continue;
+        }
+
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+
+        printf("Commande reçue : %s\n", command);
+    }
+
+    finish_main_program();
     return 0;
 }
