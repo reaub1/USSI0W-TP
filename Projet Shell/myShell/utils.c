f1f3d10dc6d3a02379bbb5271a1042a07372f1f9
Pichon Sebastien <arthox.mail@gmail.com>
2025-02-09 13:02:42 +0100

    Gestion des err avec errno, création de la méthode dans utilis.c

--- Projet Shell/myShell/utils.c
+++ Projet Shell/myShell/utils.c
@@ -1,7 +1,26 @@
 #include "utils.h"
 #include <sys/wait.h>
 #include <stddef.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+void print_error(const char *msg) {
+    if (errno != 0) {
+        fprintf(stderr, "ERR : %s - %s\n", msg, strerror(errno));
+    } else {
+        fprintf(stderr, "ERR : %s\n", msg);
+    }
+}
+
+void print_syntax_error(const char *msg) {
+    fprintf(stderr, "Erreur de syntaxe : %s\n", msg);
+}
 
 void handle_sigchld() {
-    while (waitpid(-1, NULL, WNOHANG) > 0);
+    while (waitpid(-1, NULL, WNOHANG) > 0) {
+        if (errno != ECHILD) {
+            print_error("Erreur lors de la gestion des processus zombies");
+        }
+    }
 }
